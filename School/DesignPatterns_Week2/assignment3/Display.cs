6bf233b33322d2f9c1bdd3b92177695839566169
YvanRoes <690101@student.inholland.nl>
2022-04-19 14:57:29 +0200

    .

---
+++ School/DesignPatterns_Week2/assignment3/Display.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3
+{
+    public abstract class Display
+    {
+        public Song CurrentSong { get; set; }
+
+        public Display(IObservable player) => player.Addobserver((IObserver)this);
+
+        public void update(Song song) => this.CurrentSong = song;
+
+        public abstract void DisplayData();
+    }
+}
---
+++ School/DesignPatterns_Week2/assignment3/FancyMP3Display.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment3
+{
+    public class FancyMP3Display : Display,IObserver
+    {
+        public FancyMP3Display(IObservable player)
+            : base(player) { }
+
+        public override void DisplayData() => Console.WriteLine($"{CurrentSong.Name} by {CurrentSong.Artist} ({CurrentSong.Duration:mm:ss})");
+    }
+}
