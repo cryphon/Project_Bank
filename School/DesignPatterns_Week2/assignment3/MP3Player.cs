87369d1782dfd51519d3ebed34e08aaa6678d6a7
YvanRoes <690101@student.inholland.nl>
2022-04-20 11:57:58 +0200

    .

--- School/DesignPatterns_Week2/assignment3/MP3Player.cs
+++ School/DesignPatterns_Week2/assignment3/MP3Player.cs
@@ -8,7 +8,7 @@ namespace assignment3
         public Song CurrentSong { get; private set; }
         List<Song> SongList { get; set; }
         List<IObserver> _observers = new List<IObserver>();
-        Random rnd = new Random(2);
+        Random rnd = new Random();
 
         public MP3Player()
         {
