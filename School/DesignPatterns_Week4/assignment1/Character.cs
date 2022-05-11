82709e585a15225ce01981c4db4ac494e01e5346
YvanRoes <690101@student.inholland.nl>
2022-05-11 22:39:42 +0200

    .

---
+++ School/DesignPatterns_Week4/assignment1/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    internal class Character
+    {
+        public IWeaponBehaviour Weapon;
+        public Character(){ }
+
+        public void Fight() => Weapon.UseWeapon();
+    }
+}
